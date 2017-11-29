46a3e97618127b58f7f1c219f98c863a0fd26756
Kelly Selden <kellyselden@gmail.com>
2017-11-29 11:40:38 -0800

    increase timeout

--- test/integration/index-test.js
+++ test/integration/index-test.js
@@ -6,7 +6,7 @@ const Graph = require('../../src');
 const Node = require('../../src/node');
 
 describe('Integration - index', function() {
-  this.timeout('10000');
+  this.timeout('30000');
 
   it('my-app', function() {
     let appPath = path.join(__dirname, '../fixtures/my-app');
